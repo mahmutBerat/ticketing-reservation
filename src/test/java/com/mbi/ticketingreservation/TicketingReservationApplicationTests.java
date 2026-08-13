2418999894258f32b4ad7bd40c752950c4367d48
mahmutberat-mf <mahmutberat@megafortuna.co>
2026-08-13 12:45:07 +0300

    01- Initialize project
     - define essential libraries

---
+++ src/test/java/com/mbi/ticketingreservation/TicketingReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbi.ticketingreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
